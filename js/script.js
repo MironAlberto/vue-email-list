dd17138bb45023c66f9ac987231bbc79b594d4d9
MironAlberto <alberto.miron19@gmail.com>
2024-01-10 19:19:44 +0100

    esercizio completato

--- js/script.js
+++ js/script.js
@@ -3,8 +3,15 @@ const { createApp } = Vue;
 createApp({
     data() {
         return {
-            message: 'Template HTML CSS Vue',
+            emails: [],
         };
+    },
+    mounted(){
+        for (let i = 0; i < 10; i++){
+            axios.get('https://flynn.boolean.careers/exercises/api/random/mail').then((e) => {
+                this.emails.push(e.data.response);
+                });
+        }
     }
 
 }).mount('#app')
